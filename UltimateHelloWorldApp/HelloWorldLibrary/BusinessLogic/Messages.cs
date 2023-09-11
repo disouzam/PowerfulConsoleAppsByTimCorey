d6d4054d9683cc107a3c39937f76039dd37a1a84
Dickson Souza <disouzam@yahoo.com.br>
2023-09-11 19:31:33 -0300

    Added methods LookUpCustomText and Greeting to Messages class and extracted its interface

--- UltimateHelloWorldApp/HelloWorldLibrary/BusinessLogic/Messages.cs
+++ UltimateHelloWorldApp/HelloWorldLibrary/BusinessLogic/Messages.cs
@@ -1,13 +1,55 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using System.Text.Json;
+
+using HelloWorldLibrary.Models;
+
+using Microsoft.Extensions.Logging;
 
 namespace HelloWorldLibrary.BusinessLogic;
 
-public class Messages
+public class Messages : IMessages
 {
     private readonly ILogger<Messages> log;
 
     public Messages(ILogger<Messages> log)
     {
         this.log = log;
+    }
+
+    public string Greeting(string language)
+    {
+        var output = LookUpCustomText(nameof(Greeting), language);
+        return output;
+    }
+
+    private string LookUpCustomText(string key, string language)
+    {
+        var options = new JsonSerializerOptions()
+        {
+            PropertyNameCaseInsensitive = true
+        };
+
+        try
+        {
+            var messageSets = JsonSerializer
+                .Deserialize<List<CustomText>>
+                (
+                    File.ReadAllText("CustomText.json"), options
+                );
+
+            var messages = messageSets?.First(x => x.Language == language);
+
+            if (messages is null)
+            {
+                throw new NullReferenceException("The specified language was not found in the json file.");
+            }
+
+            return messages.Translations[key];
+        }
+        catch (Exception ex)
+        {
+
+            log.LogError("Error looking up the custom text", ex);
+            throw;
+        }
     }
 }
